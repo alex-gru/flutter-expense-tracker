f78b79ceacc5f2fd0fc6366b902ab638095d9179
Alexander Gruschina <gruschina@symptoma.com>
2021-05-23 21:31:31 +0200

    hide marker while loading

--- lib/modules/marker/behind_marker_static.dart
+++ lib/modules/marker/behind_marker_static.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/modules/state/app_state.dart';
@@ -17,47 +15,50 @@ class BehindMarkerStatic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (AppStateScope.of(context).persons.isEmpty) return Container();
-    var _personBehind = AppStateScope.of(context)
+    var personBehind = AppStateScope.of(context)
         .persons
         .reduce((person, other) => person.share < other.share ? person : other);
-    var _personLeading = AppStateScope.of(context)
+    var personLeading = AppStateScope.of(context)
         .persons
         .reduce((person, other) => person.share > other.share ? person : other);
 
-    double _middle = (_personBehind.progress + _personLeading.progress) / 2;
+    var middle = (personBehind.progress + personLeading.progress) / 2;
 
-    var _left =
-        AppStateScope.of(context).persons.elementAt(0).progress < _middle
-            ? (_middle - _middle / 2)
-            : (_middle + _middle / 2);
+    var left =
+        AppStateScope.of(context).persons.elementAt(0).progress < middle
+            ? (middle - middle / 2)
+            : (middle + middle / 2);
 
-    var _diff = (_personLeading.sumExpenses + _personBehind.sumExpenses) / 2 -
-        _personBehind.sumExpenses;
+    var diff = (personLeading.progress + personBehind.progress) / 2 -
+        personBehind.sumExpenses;
 
-    if (_diff == 0) return Container();
+    var hide = personLeading.progress == personBehind.progress;
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(_left - _markerWidth / 2, 16, 0, 0),
-      child: Tooltip(
-        message: '${_personBehind.person} is ${prettifyAmount(_diff)} behind.',
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            ClipRRect(
-              child: Container(
-                width: _markerWidth,
-                height: _markerHeight,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: getPersonColor(_personBehind.person, context),
-                    width: 2,
+    return Opacity(
+      opacity: hide ? 0 : 1,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(left - _markerWidth / 2, 16, 0, 0),
+        child: Tooltip(
+          message: '${personBehind.person} is ${prettifyAmount(diff)} behind.',
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              ClipRRect(
+                child: Container(
+                  width: _markerWidth,
+                  height: _markerHeight,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: getPersonColor(personBehind.person, context),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-            ),
-            Text('- ${prettifyAmount(_diff)}')
-          ],
+              Text('- ${prettifyAmount(diff)}')
+            ],
+          ),
         ),
       ),
     );
