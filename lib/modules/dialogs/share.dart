eb31f889fb8b06e719dcdf4485223a057d2bb009
Alexander Gruschina <gruschina@symptoma.com>
2021-06-05 22:56:03 +0200

    show QR for sharing

--- lib/modules/dialogs/share.dart
+++ lib/modules/dialogs/share.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import 'dialog_result.dart';
 
@@ -15,6 +16,7 @@ class ShareDialog extends StatefulWidget {
 
 class _ShareDialogState extends State<ShareDialog> {
   final String _accountId;
+  bool _showQr = false;
 
   _ShareDialogState(this._accountId);
 
@@ -25,37 +27,70 @@ class _ShareDialogState extends State<ShareDialog> {
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                shape: StadiumBorder(),
-                side: BorderSide(width: 2, color: Theme.of(context).accentColor),
-              ),
-              onPressed: () => {log('Share now')},
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.group),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(4,0,0,0),
-                    child: Text(
-                      "Share",
-                      style: Theme.of(context).textTheme.subtitle1,
+          Visibility(
+            visible: !_showQr,
+            child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  side: BorderSide(width: 2, color: Theme.of(context).accentColor),
+                ),
+                onPressed: () {
+                  log('Share now');
+                  setState(() {
+                    _showQr = true;
+                  });
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.group),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(4,0,0,0),
+                      child: Text(
+                        "Share",
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
                     ),
-                  ),
-                ],
-              )),
+                  ],
+                )),
+          ),
+          Visibility(
+            visible: _showQr,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                QrImage(
+                  data: _accountId,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                ),
+                Text(
+                  "Scan this QR code on the other device to join.",
+                  style: Theme.of(context).textTheme.caption,
+                )
+              ],
+            ),
+          ),
           // Text(_accountId),
-          Divider(),
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                shape: StadiumBorder(),
-                side: BorderSide(width: 1, color: Theme.of(context).disabledColor),
-              ),
-              onPressed: () => {log('Leave this list now')},
-              child: Text(
-                "Leave this list",
-                style: Theme.of(context).textTheme.caption,
-              )),
+          Visibility(
+            visible: !_showQr,
+            child: Column(
+              children: [
+                Divider(),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      side: BorderSide(width: 1, color: Theme.of(context).disabledColor),
+                    ),
+                    onPressed: () => {log('Leave this list now')},
+                    child: Text(
+                      "Leave this list",
+                      style: Theme.of(context).textTheme.caption,
+                    )),
+              ],
+            ),
+          ),
+
         ],
       ),
       actions: [
