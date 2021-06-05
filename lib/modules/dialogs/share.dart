1dc652c6b54c9c773a4718ea71825e6e482645b9
Alexander Gruschina <gruschina@symptoma.com>
2021-06-06 00:46:17 +0200

    scan QR code to join existing list

--- lib/modules/dialogs/share.dart
+++ lib/modules/dialogs/share.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/modules/share/qr_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 import 'dialog_result.dart';
@@ -58,6 +59,7 @@ class _ShareDialogState extends State<ShareDialog> {
             visible: _showQr,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
                   width: 200,
@@ -66,13 +68,23 @@ class _ShareDialogState extends State<ShareDialog> {
                     data: _accountId,
                     version: QrVersions.auto,
                     size: 200.0,
-                    foregroundColor: Theme.of(context).accentColor,
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
                   ),
                 ),
-                Text(
-                  "Scan this QR code on the other device to join.",
-                  style: Theme.of(context).textTheme.caption,
-                )
+                Row(
+                  children: [
+                    Icon(Icons.info_outline),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(4, 8, 0, 0),
+                      width: 200,
+                      child: Text(
+                        "Open the Expense Tracker app on the other device and scan this QR code to join the shared list.",
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
@@ -87,9 +99,17 @@ class _ShareDialogState extends State<ShareDialog> {
                       shape: StadiumBorder(),
                       side: BorderSide(width: 1, color: Theme.of(context).disabledColor),
                     ),
-                    onPressed: () => {log('Leave this list now')},
+                    onPressed: () async {
+                      log('Open QR Scanner...');
+                      final result = await Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => QrScanner()),
+                      );
+                      log('QR code result: $result');
+                      // TODO: set accountId here
+                    },
                     child: Text(
-                      "Leave this list",
+                      "Scan QR code to join list",
                       style: Theme.of(context).textTheme.caption,
                     )),
               ],
---
+++ lib/modules/share/qr_scanner.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QrScanner extends StatefulWidget {
+  const QrScanner();
+
+  @override
+  State<StatefulWidget> createState() => _QrScannerState();
+}
+
+class _QrScannerState extends State<QrScanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR_Scanner');
+  late QRViewController controller;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Text('Open the Expense Tracker app on the other device.'
+                    'Then scan the QR code that is displayed on the other device to join the list.'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      Navigator.maybePop(context, scanData.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
