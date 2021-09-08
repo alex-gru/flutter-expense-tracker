93f28500917ef553f8522db2612600dfe2c36412
Alexander Gruschina <gruschina@symptoma.com>
2021-09-08 09:35:46 +0200

    add firebase authentication, since firestore is now allowed for authenticated users only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_expense_tracker/modules/main/home.dart';
@@ -9,7 +11,14 @@ import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
+  String? email = dotenv.env['FIREBASE_AUTH_EMAIL'];
+  String? password = dotenv.env['FIREBASE_AUTH_PASSWORD'];
+  if (email == null || password == null) {
+    throw Exception('Cannot initialize firebase - could not get auth credentials from .env file.');
+  }
+  await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
   debugPaintSizeEnabled = false;
   runApp(ExpensesApp());
 }
